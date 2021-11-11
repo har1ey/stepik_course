83a6d2adb8c610a27456047572703d0850908e91
Nickolai Vakhrushev <nickv@email.com>
2021-11-11 15:20:48 +0700

    Initial and add files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Stepik курс: 
+## [Автоматизация тестирования с помощью Selenium и Python](https://stepik.org/course/575/)
+
+Репозиторий для оценки выполнения домашнего задания темы
+[PyTest — параметризация, конфигурирование, плагины](https://stepik.org/lesson/237240/step/9)
+
+### $../stepik_course/lesson_work/test$ pytest --language=es test_items.py
+
---
+++ lesson_work/test/test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+selector = "#add_to_basket_form > button"
+
+
+def test_check_add_to_cart_button(browser):
+    browser.get(link)
+    time.sleep(5)
+    elm = browser.find_elements(By.CSS_SELECTOR, f'{selector}')
+    assert len(elm) == 1, f"Element with '{selector}' selector - not found or found more 1"
